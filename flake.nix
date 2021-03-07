173f7267f099e6670cdd2f33b4f148cd5e73462d
Andrew Jeffery <dev@jeffas.io>
2021-03-07 16:40:59 +0000

    Make flake-utils follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils }:
