a15082757fbc307ad9f5a9fb8a633b23e429400e
Andrew Jeffery <dev@jeffas.io>
2021-03-07 16:26:49 +0000

    Add flake config

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "waytext";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            system = system;
+          };
+        in
+        rec
+        {
+          packages = {
+            waytext = pkgs.stdenv.mkDerivation {
+              name = "waytext";
+              src = ./.;
+              buildInputs = with pkgs; [
+                meson
+                pkgconfig
+                cmake
+                cairo
+                wayland
+                wayland-protocols
+                scdoc
+                ninja
+              ];
+            };
+          };
+
+          defaultPackage = packages.waytext;
+
+          apps = {
+            waytext = flake-utils.lib.mkApp {
+              drv = packages.waytext;
+            };
+          };
+
+          defaultApp = apps.waytext;
+
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs;[
+              meson
+              pkgconfig
+              cmake
+              cairo
+              wayland
+              wayland-protocols
+              scdoc
+              ninja
+
+              rnix-lsp
+              nixpkgs-fmt
+            ];
+          };
+        }
+      );
+}
