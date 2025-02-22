0b4f99435add772bbd47cb2a220e627c0f2bd2c6
Andrew Jeffery <dev@jeffas.net>
2025-02-22 16:22:43 +1100

    Use flake-utils to make flake for each system

--- flake.nix
+++ flake.nix
@@ -3,50 +3,45 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-  in rec
-  {
-    packages.${system} = {
-      waytext = pkgs.stdenv.mkDerivation {
-        name = "waytext";
-        src = ./.;
-        buildInputs = with pkgs; [
-          meson
-          pkg-config
-          cmake
-          cairo
-          wayland
-          wayland-protocols
-          wayland-scanner
-          scdoc
-          ninja
-        ];
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages = {
+        waytext = pkgs.stdenv.mkDerivation {
+          name = "waytext";
+          src = ./.;
+          buildInputs = with pkgs; [
+            meson
+            pkg-config
+            cmake
+            cairo
+            wayland
+            wayland-protocols
+            wayland-scanner
+            scdoc
+            ninja
+          ];
+        };
       };
-    };
 
-    apps.${system} = {
-      waytext = flake-utils.lib.mkApp {
-        drv = packages.${system}.waytext;
+      apps = {
+        waytext = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.waytext;
+        };
       };
-    };
 
-    overlays.default = final: _prev: self.packages.${system};
+      overlays.default = final: _prev: self.packages.${system};
 
-    devShells.${system}.default = pkgs.mkShell {
-      inputsFrom = [self.packages.${system}.waytext];
-    };
-  };
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [self.packages.${system}.waytext];
+      };
+    });
 }
